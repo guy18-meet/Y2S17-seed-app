decfc0a8e6d7fa75e2589f86af49654f4e618dda
Student <selaguy88@gmail.com>
2017-08-01 15:45:31 +0300

    fix and add templates

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 # SQLAlchemy
-from model import Base, YourModel
+from model import Base, Post, Options
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -18,24 +18,18 @@ session = DBSession()
 @app.route('/')
 def my_feed():
     
-	comps = session.query(Comp).all()
-    return render_template('my_feed.html', comps=comps)
+    # comps = session.query(Comp).all()
+    comps = [{"":""}]
+    return render_template('myfeed.html', comps=comps)
 
-@app.route('/<str:category>')
+@app.route('/<string:category>')
 def my_feed_category():
-    
-	comps = session.query(Comp).filter_by(category=category).all()
-    return render_template('my_feed.html', comps=comps)
-    
+    comps = session.query(Comp).filter_by(category=category).all()
+    return render_template('myfeed.html', comps=comps)
 
 
 
 @app.route ('/',methods=['GET', 'POST'])
 def add_poll():
-	 return render_template('addpoll.html')
-	pass
-
-
-@app.route('/pass')
-    def random():
-        pass
+    return render_template('addpoll.html')
+    pass
--- model.py
+++ model.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, DateTime, Integer, String, Boolean
+from sqlalchemy import Column, DateTime, Integer, String, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
