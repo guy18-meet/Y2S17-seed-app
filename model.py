22df3806a935878c8fe44be067b95283e21f6480
guy18-meet <selaguy88@gmail.com>
2017-08-02 14:42:45 +0300

    sh

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, DateTime, Integer, String, Boolean, ForeignKey
-from sqlalchemy.ext.declaratiove import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
