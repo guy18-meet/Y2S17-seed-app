2c45742c7111399be6d8fb2009f046326bfb6d61
guy18-meet <selaguy88@gmail.com>
2017-08-02 11:00:43 +0300

    jeez

--- model.py
+++ model.py
@@ -1,27 +1,27 @@
 from sqlalchemy import Column, DateTime, Integer, String, Boolean, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declaratiove import declarative_base
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 class Post(Base):
     __tablename__  = 'post'
-    id = Column(Integer, primary_key=True)
-    category= Column(String)
-    title= Column(String(100))
-    description= Column(String(140))
-    votes= Column(Integer, default=0)
-    option_id = relationship("Option")
+    id          = Column(Integer, primary_key=True)
+    category    = Column(String)
+    title       = Column(String(100))
+    description = Column(String(140))
+    votes       = Column(Integer, default=0)
+    options     = relationship("Option")
     
     # ADD YOUR FIELD BELOW I
 
 class Option(Base):
-    __tablename__  = 'option'
-    id = Column(Integer, primary_key=True)
-    option= Column(String(30))
-    pic_url=Column(String)
-    opvote=Column(Integer, default=0)
-    post_id=Column(Integer, ForeignKey('post.id'))
+    __tablename__ = 'option'
+    id      = Column(Integer, primary_key=True)
+    option  = Column(String(30))
+    pic_url = Column(String)
+    opvote  = Column(Integer, default=0)
+    post    = Column(Integer, ForeignKey('post.id'))
     
 # IF YOU NEED TO CREATE OTHER TABLE 
 
